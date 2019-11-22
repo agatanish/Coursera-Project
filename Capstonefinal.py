8532a00e0ca1ec3a600392bfff2a6521d0677afe
agatanish <agatanish@gmail.com>
2019-11-22 21:36:29 +0530

    Update Capstonefinal.py

--- Capstonefinal.py
+++ Capstonefinal.py
@@ -32,14 +32,14 @@ header = Label(window, text="Hospital Database System", font=("arial",20,"bold")
 
 
 
-L1 = Label(window, text = "PATIENT INFO", font=("arial", 14)).place(x=10,y=50)
-L2 = Label(window, text = "Patient Name", font=("arial",14)).place(x=10,y=75)
-L3 = Label(window, text = "Patient ID", font=("arial",14)).place(x=10,y=100)
-L4 = Label(window, text = "Age", font=("arial",14)).place(x=10,y=125)
-L5 = Label(window, text = "Sex", font=("arial",14)).place(x=10,y=150)
-L6 = Label(window, text = "Blood Group", font=("arial",14)).place(x=10,y=175)
-L7=  Label(window, text = "Contact Info", font=("arial",14)).place(x=10,y=200)
-L8=  Label(window, text=  "Email-Id", font=("arial",14)).place(x=10,y=225)
+L1 = Label(window, text = "PATIENT INFO", font=("arial", 12)).place(x=10,y=30)
+L2 = Label(window, text = "Patient Name", font=("arial",12)).place(x=10,y=55)
+L3 = Label(window, text = "Patient ID", font=("arial",12)).place(x=10,y=80)
+L4 = Label(window, text = "Age", font=("arial",12)).place(x=10,y=105)
+L5 = Label(window, text = "Sex", font=("arial",12)).place(x=10,y=130)
+L6 = Label(window, text = "Blood Group", font=("arial",12)).place(x=10,y=155)
+L7=  Label(window, text = "Contact Info", font=("arial",12)).place(x=10,y=180)
+L8=  Label(window, text=  "Email-Id", font=("arial",12)).place(x=10,y=205)
 
 
 # In[3]:
@@ -58,25 +58,25 @@ email=StringVar(window)
 
 
 nameT = Entry(window, textvariable=name)
-nameT.place(x=220,y=80)
+nameT.place(x=220,y=55)
 
 idT = Entry(window, textvariable=id)
-idT.place(x=220,y=105)
+idT.place(x=220,y=80)
 
 ageT = Entry(window, textvariable=age)
-ageT.place(x=220,y=130)
+ageT.place(x=220,y=105)
 
 sexT = Entry(window, textvariable=sex)
-sexT.place(x=220,y=155)
+sexT.place(x=220,y=130)
 
 bloodT = Entry(window, textvariable=blood)
-bloodT.place(x=220,y=180)
+bloodT.place(x=220,y=155)
 
 contactT=Entry(window,textvariable=contact)
-contactT.place(x=220,y=205)
+contactT.place(x=220,y=180)
 
 emailT=Entry(window,textvariable=email)
-emailT.place(x=220,y=230)
+emailT.place(x=220,y=205)
 
 
 # In[5]:
@@ -228,40 +228,43 @@ def message():
         if(oldState != newState):
 
             if(newState == 1):
-                label1 = Label(root, text="Message 1", width=10, fg='white', bg='black',font=("Helvetica", 44)).place(x=80,y=120)
+                label1 = Label(root, text="Yes", width=10, fg='white', bg='black',font=("Helvetica", 44)).place(x=80,y=120)
                 oldState=newState
-                sendSMS("message 1 doctor")
+                sendSMS("yes, Doctor")
             elif(newState == 2):
-                label1 = Label(root, text="Message 2", width=10, fg='white', bg='black',font=("Helvetica", 44)).place(x=80,y=120)
-                sendSMS("message 2 doctor")
+                label1 = Label(root, text="No", width=10, fg='white', bg='black',font=("Helvetica", 44)).place(x=80,y=120)
+                sendSMS("No, Doctor")
                 oldState=newState
             elif(newState == 3):
-                label1 = Label(root, text="Message 3", width=10, fg='white', bg='black',font=("Helvetica", 44)).place(x=80,y=120)
-                sendSMS("message 3 doctor")
+                label1 = Label(root, text="Need Water", width=10, fg='white', bg='black',font=("Helvetica", 44)).place(x=80,y=120)
+                sendSMS("Need Water, Doctor")
                 oldState=newState
             elif(newState == 4):
-                label1 = Label(root, text="Message 4", width=10, fg='white', bg='black',font=("Helvetica", 44)).place(x=80,y=120)
-                sendSMS("message 4 doctor")
+                label1 = Label(root, text="Washroom", width=10, fg='white', bg='black',font=("Helvetica", 44)).place(x=80,y=120)
+                sendSMS("Washroom, Doctor")
                 oldState=newState
             elif(newState == 5):
-                label1 = Label(root, text="Message 5", width=10, fg='white', bg='black',font=("Helvetica", 44)).place(x=80,y=120)
-                sendSMS("message 5 doctor")
+                label1 = Label(root, text="Emergency", width=10, fg='white', bg='black',font=("Helvetica", 44)).place(x=80,y=120)
+                sendSMS("Emergency, Doctor")
                 #time.sleep(120)
                 oldState=newState
             elif(newState == 6):
                 #var1.set("message 6")
-                label1 = Label(root, text="Message 6", width=10, fg='white', bg='black',font=("Helvetica", 44)).place(x=80,y=120)
+                label1 = Label(root, text="Need food", width=10, fg='white', bg='black',font=("Helvetica", 44)).place(x=80,y=120)
                 #time.sleep(120)
+                sendSMS("Need Food , Doctor")
                 oldState=newState
             elif(newState == 7):
                 #var1.set("message 7")
-                label1 = Label(root, text="Message 7", width=10, fg='white', bg='black',font=("Helvetica", 44)).place(x=80,y=120)
+                label1 = Label(root, text="Feel Pain", width=10, fg='white', bg='black',font=("Helvetica", 44)).place(x=80,y=120)
                 #time.sleep(120)
+                sendSMS("Feel Pain , Doctor")
                 oldState=newState
             elif(newState == 8):
                 #var1.set("message 8")
-                label1 = Label(root, text="Message 8", width=10, fg='white', bg='black',font=("Helvetica", 44)).place(x=80,y=120)
+                label1 = Label(root, text="Feeling cold", width=10, fg='white', bg='black',font=("Helvetica", 44)).place(x=80,y=120)
                 #time.sleep(120)
+                sendSMS("Feeling , Doctor")
                 oldState=newState
 
 
@@ -289,20 +292,20 @@ def message():
 
 
 button_1 = Button(window, text="Submit",command=get)
-button_1.place(x=100,y=260)
+button_1.place(x=100,y=235)
 
 button_2 = Button(window,text= "Clear",command=clear)
-button_2.place(x=10,y=260)
+button_2.place(x=10,y=235)
 button_3 = Button(window,text="Open DB",command=record)
-button_3.place(x=10,y=290)
+button_3.place(x=10,y=260)
 button_4= Button(window,text="Delete Entry",command=delete)
-button_4.place(x=190,y=260)
+button_4.place(x=190,y=235)
 
 button_5=Button(window,text="Message",command=message)
-button_5.place(x=100,y=290)
+button_5.place(x=100,y=260)
 
 button_6=Button(window,text="Visualisation")
-button_6.place(x=190,y=290)
+button_6.place(x=190,y=260)
 
 
 # In[12]:
